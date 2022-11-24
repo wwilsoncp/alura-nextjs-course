abb2dba142e3bf53f01b9268cfae72c6af5a36ac
Wilson Paiva <paiva.wilson@dedalus.com>
2022-11-24 20:04:16 -0300

    Adicionando estilo nas páginas

---
+++ pages/faq.js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import Link from "../src/components/Link";
+
+// export async function getServerSideProps(context) {
+//   console.log('Em Mode DEV (yarn start) sempre executa esse método')
+//   console.log('Roda a cada acesso que o app recebe em PROD')
+// }
+
+export async function getStaticProps() {
+  console.log('Em Mode DEV (yarn start) sempre executa esse método')
+  console.log('Roda SOMENTE no BUILD (yarn build) do projeto')
+  const URL_API = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+  const faq = await fetch(URL_API)
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log('data', data)
+      return data;
+    })
+  return {
+    props: {
+      faq
+    }, // will be passed to the page component as props
+  }
+}
+
+export default function FaqPage(props) {
+  console.log('props', props);
+  return (
+    <div>
+      <Head>
+        <title>Faq Alura Cases</title>
+      </Head>
+      <h1>Alura Cases - Perguntas</h1>
+      <Link href="/">
+        Home
+      </Link>
+      <ul>
+        {props?.faq?.map(({ answer, question }) => {
+          return (
+            <li key={question}>
+              <article>
+                <h2>{question}</h2>
+                <p>{answer}</p>
+
+              </article>
+            </li>)
+        }
+        )
+        }
+
+      </ul>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,38 @@
-import Link from "../src/components/Link";
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Alura - Page</h1>
-      <Link href="/ref">
-        Ir Para Ref
-      </Link>
-    </div>
-  )
-}
+export default HomeScreen;
+
+// import React from "react";
+// import Footer from "../src/components/Footer";
+// import Link from "../src/components/Link";
+
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return (
+//     <React.Fragment>
+//       <Tag>
+//         {children}
+//       </Tag>
+//       <style jsx>
+//         {`
+//           ${Tag} {
+//             color: red;
+//           }
+//         `}
+//       </style>
+//     </React.Fragment>
+//   )
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h1">Alura - Page</Title>
+//       <Title as="h2">Entre e veja tudo sobre React/Next</Title>
+//       <Link href="/ref">
+//         Ir Para Ref
+//       </Link>
+//       <Footer />
+//     </div>
+//   )
+// }
